d5f2a0886d42c1ebeb1f712e8fbae0c5270f261a
quangnguyen37 <quanghng37@gmail.com>
2023-07-05 18:53:31 -0700

    Import pandas dataframe and plot using justpy

--- 0-simple-app.py
+++ 0-simple-app.py
@@ -4,6 +4,7 @@ def app():
     wp = jp.QuasarPage()
     h1 = jp.QDiv(a=wp, text = "Analysis of Course Reviews", classes = "text-h3 text-center q-pa-md")
     p1 = jp.QDiv(a=wp, text = "These graphs represent course review analysis")
+    
 
     return wp
 
